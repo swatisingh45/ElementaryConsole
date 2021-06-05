843dd5616195446a5a87e1c052f4cc57a1cf0540
Swati Singh <swsingh3@cisco.com>
2021-06-05 21:57:17 +0530

    Final added

---
+++ README.md
@@ -0,0 +1,31 @@
+# WebEx_ElementryConsole
+
+
+Elementary console for simplified Meeting experience
+
+Overview- To create an elementary console for Virtual meeting platform. This console is targeted to help people who are non-tech savvy, have language barriers and are visually disabled.
+The elementary console will have only the essential options to control the meeting i.e. Mute/Unmute and Camera On/Off. These options can be represented in numerous languages as the user selects. 
+The elementary console can also have a voice interpreter which will execute commands such as ‘Mute/unmute audio’ and ‘Turn on/off video’
+
+
+
+
+
+Elementary console for simplified Meeting experience
+
+
+
+
+
+
+
+
+
+
+Requirements-
+
+•	An SDK for the elementary console which can be installed and run on the PC
+•	When the user joins a meeting, the elementary console pops up and lets the user navigate the meeting using the controls on this console
+•	User can configure which options he wants to see on the elementary console
+•	User can configure the language in which the options are displayed in the elementary console.
+•	To be developed with WebEx meeting platform 
---
+++ index.js
@@ -0,0 +1,129 @@
+const myAccessToken = 'MjVmYTViNTEtOGQwMi00MDhhLWJkYWUtOGQwOGZiMTExOGVkMGNhNTUxYWUtNjQ0_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f';
+
+if (myAccessToken === 'MjVmYTViNTEtOGQwMi00MDhhLWJkYWUtOGQwOGZiMTExOGVkMGNhNTUxYWUtNjQ0_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f') {
+  alert('Make sure to update your access token in the index.js file!');
+  return;
+}
+
+const webex = window.Webex.init({
+  credentials: {
+    access_token: myAccessToken
+  }
+});
+
+webex.meetings.register()
+  .catch((err) => {
+    console.error(err);
+    alert(err);
+    throw err;
+  });
+
+function bindMeetingEvents(meeting) {
+  meeting.on('error', (err) => {
+    console.error(err);
+  });
+
+  // Handle media streams changes to ready state
+  meeting.on('media:ready', (media) => {
+    if (!media) {
+      return;
+    }
+    if (media.type === 'local') {
+      document.getElementById('self-view').srcObject = media.stream;
+    }
+    if (media.type === 'remoteVideo') {
+      document.getElementById('remote-view-video').srcObject = media.stream;
+    }
+    if (media.type === 'remoteAudio') {
+      document.getElementById('remote-view-audio').srcObject = media.stream;
+    }
+  });
+
+  // Handle media streams stopping
+  meeting.on('media:stopped', (media) => {
+    // Remove media streams
+    if (media.type === 'local') {
+      document.getElementById('self-view').srcObject = null;
+    }
+    if (media.type === 'remoteVideo') {
+      document.getElementById('remote-view-video').srcObject = null;
+    }
+    if (media.type === 'remoteAudio') {
+      document.getElementById('remote-view-audio').srcObject = null;
+    }
+  });
+
+  // Of course, we'd also like to be able to leave the meeting:
+  document.getElementById('hangup').addEventListener('click', () => {
+    meeting.leave();
+  });
+
+  document.getElementById('mic').addEventListener('click', function () {
+    alert("HI");
+  });
+  
+}
+
+// Join the meeting and add media
+function joinMeeting(meeting) {
+
+  return meeting.join().then(() => {
+    const mediaSettings = {
+      receiveVideo: true,
+      receiveAudio: true,
+      receiveShare: false,
+      sendVideo: true,
+      sendAudio: true,
+      sendShare: false
+    };
+
+    // Get our local media stream and add it to the meeting
+    return meeting.getMediaStreams(mediaSettings).then((mediaStreams) => {
+      const [localStream, localShare] = mediaStreams;
+
+      meeting.addMedia({
+        localShare,
+        localStream,
+        mediaSettings
+      });
+    });
+  });
+}
+
+document.getElementById('destination').addEventListener('submit', (event) => {
+  // again, we don't want to reload when we try to join
+  event.preventDefault();
+
+  const destination = document.getElementById('invitee').value;
+
+  return webex.meetings.create(destination).then((meeting) => {
+    // Call our helper function for binding events to meetings
+    bindMeetingEvents(meeting);
+
+    return joinMeeting(meeting);
+  })
+  .catch((error) => {
+    // Report the error
+    console.error(error);
+  });
+
+
+
+});
+
+
+    
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+} 
